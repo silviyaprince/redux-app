6d122bdec434574ff1184fccc34230eea8ea6b1a
silviyaprince <silviya.prince16@gmail.com>
2024-10-20 16:51:52 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { Counter } from './Counter';
+import { Cartpage } from './Cartpage';
 
 function App() {
   return (
     <div className="App">
-     <Counter/>
+     {/* <Counter/> */}
+     <Cartpage/>
     </div>
   );
 }
---
+++ src/Cartpage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export  function Cartpage() {
+  return (
+    <div>
+        <div className='flex-container'>
+            <div className='flex-top'>
+            <div><img src="https://res.cloudinary.com/bloomsbury-atlas/image/upload/w_360,c_scale,dpr_1.5/jackets/9781408855652.jpg" alt="book image" style={{height:"200px",widht:"100px"}}/></div>
+            <div><h3>HARRY POTTER</h3></div>
+            <div></div>
+            <div></div>
+            </div>
+        </div>
+    </div>
+  )
+}
